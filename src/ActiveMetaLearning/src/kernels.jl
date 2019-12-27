11702c61c3192af40528a3a7fa0f2258998f1017
Isak Falk <me@isakfalk.com>
2019-12-27 18:24:30 +0100

    Add MMD distribution kernel

--- src/ActiveMetaLearning/src/kernels.jl
+++ src/ActiveMetaLearning/src/kernels.jl
@@ -25,19 +25,20 @@ const MMD = MaximumMeanDiscrepancy
 function (dist::MaximumMeanDiscrepancy)(p::CategoricalDistribution, q::CategoricalDistribution)
     X = p.xs
     α = reshape(p.p, :, 1)
-    K_XX = kernelmatrix(dist.k, X, obsdim=1)
+    K_XX = KernelFunctions.kernelmatrix(dist.k, X, obsdim=1)
     αᵀKα = α'*K_XX*α
 
     Y = q.xs
     β = reshape(q.p, :, 1)
-    K_YY = kernelmatrix(dist.k, Y, obsdim=1)
+    K_YY = KernelFunctions.kernelmatrix(dist.k, Y, obsdim=1)
     βᵀKβ = β'*K_YY*β
 
-    K_XY = kernelmatrix(dist.k, X, Y, obsdim=1)
+    K_XY = KernelFunctions.kernelmatrix(dist.k, X, Y, obsdim=1)
     αᵀKβ = α'*K_XY*β
-
-    # Return the MMD.
-    return (αᵀKα + βᵀKβ - 2αᵀKβ)[1]
+    mmd² = (αᵀKα + βᵀKβ - 2αᵀKβ)[1, 1]
+    @assert mmd² + 10 * eps(eltype(mmd²)) >= 0.0, "The value of mmd² should be non-negative, but is $(mmd²)"
+    mmd = .√(mmd² + 10 * eps(eltype(mmd²)))
+    return mmd
 end
 
 """
@@ -45,31 +46,33 @@ Calculate the mmd pairwise between two vectors `ps` and `qs` of
 `CategoricalDistribution`'s. We do this efficiently by using the fact that,
 letting K^{i, j} being the kernel matrix between `p[i].xs` and `q[j].xs` and denoting
 `p[i].p` by `σ^i` and `q[j].p` by `β^j`, we can write
-    mmd_grammian_{i, j} = α^i'*K^{i, i}*α^i + β^j'*K^{j, j}*β^j - 2α^i'*K^{i, j}*β^j
+    mmd²_grammian_{i, j} = α^i'*K^{i, i}*α^i + β^j'*K^{j, j}*β^j - 2α^i'*K^{i, j}*β^j
 and see that the first and second term can be broadcasted over the columns and rows respectively.
+
+Return the mmd_grammian (square root of mmd²_grammian).
 """
 function (dist::MaximumMeanDiscrepancy)(ps::Vector{CategoricalDistribution}, qs::Vector{CategoricalDistribution})
     p_len = length(ps)
     q_len = length(qs)
-    mmd_grammian = zero(eltype(ps[1].xs), p_len, q_len)
+    mmd²_grammian = zeros(eltype(ps[1].xs), p_len, q_len)
     # Broadcast over columns
     for i ∈ range(1, stop=length(ps))
         X = ps[i].xs
         α = reshape(ps[i].p, :, 1)
-        K_XX = kernelmatrix(dist.k, X, obsdim=1)
+        K_XX = KernelFunctions.kernelmatrix(dist.k, X, obsdim=1)
         αᵀKα = α'*K_XX*α
         for j ∈ range(1, stop=length(qs))
-            mmd_grammian[i, j] += αᵀKα
+            mmd²_grammian[i, j] += αᵀKα[1, 1]
         end
     end
     # Broadcast over rows
     for j ∈ range(1, stop=length(qs))
         Y = qs[j].xs
         β = reshape(qs[j].p, :, 1)
-        K_YY = kernelmatrix(dist.k, Y, obsdim=1)
+        K_YY = KernelFunctions.kernelmatrix(dist.k, Y, obsdim=1)
         βᵀKβ = β'*K_YY*β
         for i ∈ range(1, stop=length(ps))
-            mmd_grammian[i, j] += βᵀKβ
+            mmd²_grammian[i, j] += βᵀKβ[1, 1]
         end
     end
     # Calculate α'*K_XY*β
@@ -79,11 +82,13 @@ function (dist::MaximumMeanDiscrepancy)(ps::Vector{CategoricalDistribution}, qs:
             α = reshape(ps[i].p, :, 1)
             Y = qs[j].xs
             β = reshape(qs[j].p, :, 1)
-            K_XY = kernelmatrix(dist.k, X, Y, obsdim=1)
+            K_XY = KernelFunctions.kernelmatrix(dist.k, X, Y; obsdim=1)
             αᵀKβ = α'*K_XY*β
-            mmd_grammian[i, j] -= 2αᵀKβ
+            mmd²_grammian[i, j] -= 2αᵀKβ[1, 1]
         end
     end
+    @assert all(mmd²_grammian .+ 10 * eps(eltype(mmd²_grammian)) .>= 0.0) "The values of the grammian should be non-negative but the smallest value is $(minimum(mmd²_grammian))"
+    mmd_grammian = .√(mmd²_grammian .+ 10 * eps(eltype(mmd²_grammian)))
     return mmd_grammian
 end
 
@@ -96,41 +101,32 @@ letting K^{i, j} being the kernel matrix between `p[i].xs` and `p[j].xs` and den
 and see that the first and second term can be broadcasted over the columns and rows respectively.
 """
 function (dist::MaximumMeanDiscrepancy)(ps::Vector{CategoricalDistribution})
-    p_len = length(ps)
-    mmd_grammian = zero(eltype(ps[1].xs), p_len, p_len)
-    # Broadcast over columns
-    for j ∈ range(2, stop=length(ps))
-        for i ∈ range(2, stop=j)
-            X = ps[i].xs
-            α = reshape(ps[i].p, :, 1)
-            K_XX = kernelmatrix(dist.k, X, obsdim=1)
-            αᵀKα = α'*K_XX*α
-            for j ∈ range(1, stop=length(qs))
-                mmd_grammian[i, j] += αᵀKα
-            end
-        end
-    end
-    # Broadcast over rows
-    for j ∈ range(1, stop=length(qs))
-        Y = qs[j].xs
-        β = reshape(qs[j].p, :, 1)
-        K_YY = kernelmatrix(dist.k, Y, obsdim=1)
-        βᵀKβ = β'*K_YY*β
-        for i ∈ range(1, stop=length(ps))
-            mmd_grammian[i, j] += βᵀKβ
-        end
-    end
-    # Calculate α'*K_XY*β
-    for j ∈ range(1, stop=length(qs))
-        for i ∈ range(1, stop=length(ps))
-            X = ps[i].xs
-            α = reshape(ps[i].p, :, 1)
-            Y = qs[j].xs
-            β = reshape(qs[j].p, :, 1)
-            K_XY = kernelmatrix(dist.k, X, Y, obsdim=1)
-            αᵀKβ = α'*K_XY*β
-            mmd_grammian[i, j] -= 2αᵀKβ
-        end
-    end
-    return mmd_grammian
+    # In future could speed up by leveraging symmetry of
+    # Grammian and reuse of terms in MMD calculation
+    return dist(ps, ps)
+end
+
+####################################################
+# Kernel on distributions (conv. comb. of diracs)  #
+# we follow KernelFunctions to the extent possible #
+####################################################
+
+"""
+The MMDKernel calculates kernels on CategoricalDistribution.
+
+Would want to use 2 kernels, but will use the base_κ as the original used in MMD
+and then the mapping for the second one
+"""
+struct MMDKernel{K<:Kernel, V<:Kernel}
+    base_k::K
+    higher_k::V
+end
+
+# Will have to hardcode this until there is a way to use Distances.jl on the CategoricalDistribution
+function KernelFunctions.kernelmatrix(k::MMDKernel{<:Kernel, SqExponentialKernel{T, Tr}}, ps::Vector{CategoricalDistribution}, qs::Vector{CategoricalDistribution}) where {T, Tr<:Transform}
+    mmd = MMD(k.base_k)
+    mmd_grammian = mmd(ps, qs)
+    # Require transform to be scalar Kernel.ScaleTransform
+    ρ²mmd²_grammian = transform(k.higher_k.transform, mmd_grammian).^2
+    return exp.(-ρ²mmd²_grammian)
 end
