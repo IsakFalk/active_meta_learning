1ac48653c9a7efc8565bead18ca4636c43b2f2e5
Isak Falk <me@isakfalk.com>
2019-12-27 18:25:07 +0100

    Update ActiveMetaLearning.jl exports

--- src/ActiveMetaLearning/src/ActiveMetaLearning.jl
+++ src/ActiveMetaLearning/src/ActiveMetaLearning.jl
@@ -1,15 +1,23 @@
 module ActiveMetaLearning
 
 include("tasks.jl")
-include("maml.jl")
-include("kernels.jl")
-
-export sample_task, get_array_of_tasks
 export MetaLearningEnvironment, MetaLearningRegEnvironment, MetaLearningClassEnvironment
+export sine_wave_environment, sine_wave_linear_mixture_environment
+export common_mean_ex1_environment, common_mean_ex2_environment
 export low_dim_lin_reg_environment, isotropic_lin_reg_environment
+export sample_task, get_array_of_tasks
 
+include("meta_learning.jl")
 export fomaml
+export learning_to_learn_around_a_common_mean, learning_to_learn_around_a_common_mean_r0
 
-export CategoricalDistribution, MaximumMeanDiscrepancy
+include("kernels.jl")
+export MaximumMeanDiscrepancy, MMDKernel, kernelmatrix
+
+include("optimisation.jl")
+export EmpiricalDistributionKernelFrankWolfe, EmpiricalDistributionKernelHerding, optimize!
+
+include("distributions.jl")
+export CategoricalDistribution
 
 end
